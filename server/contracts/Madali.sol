b022a1da71488ca1a38e88c36f93c97b535516f9
rowpacmal <rowpacmal@outlook.com>
2024-12-02 15:20:31 +0100

    added contract structs

--- server/contracts/Madali.sol
+++ server/contracts/Madali.sol
@@ -3,6 +3,29 @@ pragma solidity ^0.8.27;
 
 contract Madali {
     // Structs
+    struct Class {
+        address teacher;
+        address[] students;
+        bool exist;
+    }
+
+    struct Course {
+        address teacher;
+        bool exist;
+        uint256 class;
+    }
+
+    struct Student {
+        bool exist;
+        mapping(uint256 => mapping(uint256 => uint256)) grades;
+        uint256 class;
+    }
+
+    struct Teacher {
+        bool exist;
+        uint256 class;
+        uint256[] courses;
+    }
 
     // Variables
     address public admin;

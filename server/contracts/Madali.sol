0062ecb46c8cd8e1e9aac63da960054bd8e42299
rowpacmal <rowpacmal@outlook.com>
2024-12-02 15:19:45 +0100

    created base contract

---
+++ server/contracts/Madali.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
+
+contract Madali {
+    // Structs
+
+    // Variables
+    address public admin;
+
+    // Mappings
+
+    // Events
+
+    // Errors
+
+    // Constructor
+    constructor() {
+        admin = msg.sender;
+    }
+
+    // Modifiers
+
+    // Functions
+}

712c39c296acf2dacdb5c87c4cebf878f6c3f0fa
rowpacmal <rowpacmal@outlook.com>
2024-12-11 13:03:04 +0100

    added TS typing on hardhat compile with @typechain

--- server/hardhat.config.ts
+++ server/hardhat.config.ts
@@ -1,8 +1,11 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+import '@typechain/hardhat';
+import '@nomicfoundation/hardhat-ethers';
+import '@nomicfoundation/hardhat-chai-matchers';
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.27",
+  solidity: '0.8.27',
 };
 
 export default config;

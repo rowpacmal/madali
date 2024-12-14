1f34520a6a1e77c04c3443491a47a2699a2dc9c1
rowpacmal <rowpacmal@outlook.com>
2024-12-14 12:18:02 +0100

    added test for education certificate contract are deployed correctly

--- server/test/Factory.ts
+++ server/test/Factory.ts
@@ -1,5 +1,6 @@
 import { expect } from 'chai';
 import {
+  EducationCertificate,
   Factory,
   GradingSystem,
   StudentManagement,
@@ -12,11 +13,18 @@ describe('Factory Deployment and Contract Interaction', function () {
   let studentManagement: StudentManagement;
   let teacherManagement: TeacherManagement;
   let gradingSystem: GradingSystem;
+  let educationCertificate: EducationCertificate;
   let owner: any;
 
   beforeEach(async function () {
-    ({ owner, factory, studentManagement, teacherManagement, gradingSystem } =
-      await deployContractFixture());
+    ({
+      owner,
+      factory,
+      studentManagement,
+      teacherManagement,
+      gradingSystem,
+      educationCertificate,
+    } = await deployContractFixture());
   });
 
   describe('Deploying and Linking Contracts', function () {
@@ -49,12 +57,33 @@ describe('Factory Deployment and Contract Interaction', function () {
       );
     });
 
+    it('should deploy the EducationCertificate contract with correct parameters', async function () {
+      // Verify that the EducationCertificate contract was deployed with the correct addresses
+      const educationCertificateGradingSystemContract =
+        await educationCertificate.gradingContract();
+      const educationCertificateStudentContract =
+        await educationCertificate.studentContract();
+      const educationCertificateTeacherContract =
+        await educationCertificate.teacherContract();
+
+      expect(educationCertificateGradingSystemContract).to.equal(
+        await gradingSystem.getAddress()
+      );
+      expect(educationCertificateStudentContract).to.equal(
+        await studentManagement.getAddress()
+      );
+      expect(educationCertificateTeacherContract).to.equal(
+        await teacherManagement.getAddress()
+      );
+    });
+
     it('should set the contract owner to the same address for all contracts', async function () {
       // Verify that the contract owner is set to the same address.
       expect(await factory.owner()).to.equal(owner.address);
       expect(await studentManagement.owner()).to.equal(owner.address);
       expect(await teacherManagement.owner()).to.equal(owner.address);
       expect(await gradingSystem.owner()).to.equal(owner.address);
+      expect(await educationCertificate.owner()).to.equal(owner.address);
     });
   });
 });

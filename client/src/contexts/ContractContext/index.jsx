6221e80337f12cd7048979b394120bfceb170b06
rowpacmal <rowpacmal@outlook.com>
2024-12-16 15:57:34 +0100

    updated the contract context to load the abi's and set the user wallet address and role for global use

--- client/src/contexts/ContractContext/index.jsx
+++ client/src/contexts/ContractContext/index.jsx
@@ -1,34 +1,97 @@
 import { createContext, useEffect, useState } from 'react';
-import initializeContract from '../../utils/initializeContract';
 import educationCertificate from '../../utils/educationCertificate.config';
 import gradingSystem from '../../utils/gradingSystem.config';
 import studentManagement from '../../utils/studentManagement.config';
 import teacherManagement from '../../utils/teacherManagement.config';
+import { ethers } from 'ethers';
 
 const ContractContext = createContext({});
 
-const ContractProvider = ({ children }) => {
+function ContractProvider({ children }) {
   const [certificateContract, setCertificateContract] = useState(null);
   const [gradingContract, setGradingContract] = useState(null);
   const [studentContract, setStudentContract] = useState(null);
   const [teacherContract, setTeacherContract] = useState(null);
+  const [userRole, setUserRole] = useState('');
+  const [walletAddress, setWalletAddress] = useState('');
 
   useEffect(() => {
-    (async () => {
-      await handleContractState(educationCertificate, setCertificateContract);
-      await handleContractState(gradingSystem, setGradingContract);
-      await handleContractState(studentManagement, setStudentContract);
-      await handleContractState(teacherManagement, setTeacherContract);
+    (async function () {
+      await handleContractStates(
+        [
+          educationCertificate,
+          gradingSystem,
+          studentManagement,
+          teacherManagement,
+        ],
+        [
+          setCertificateContract,
+          setGradingContract,
+          setStudentContract,
+          setTeacherContract,
+        ]
+      );
     })();
   }, []);
 
-  async function handleContractState(contact, setState) {
-    const { readContract, writeContract } = await initializeContract(contact);
+  useEffect(() => {
+    if (!gradingContract) {
+      return;
+    }
 
-    setState({
-      read: readContract,
-      write: writeContract,
-    });
+    (async function () {
+      const role = Number(await gradingContract.read.getUserRole());
+
+      switch (role) {
+        case 0:
+          setUserRole('Admin');
+          break;
+
+        case 1:
+          setUserRole('Teacher');
+          break;
+
+        case 2:
+          setUserRole('Student');
+          break;
+
+        default:
+          setUserRole('Unauthorized');
+          break;
+      }
+    })();
+  }, [gradingContract]);
+
+  async function handleContractStates(contracts, setStates) {
+    let provider,
+      signer = null;
+
+    try {
+      provider = new ethers.BrowserProvider(window.ethereum);
+      signer = await provider.getSigner();
+
+      setWalletAddress(await signer.getAddress());
+    } catch (error) {
+      console.error('Error initializing the provider : ', error);
+    }
+
+    for (let i = 0; i < contracts.length; i++) {
+      const { name, abi, address } = contracts[i];
+      let readContract,
+        writeContract = null;
+
+      try {
+        readContract = new ethers.Contract(address, abi, provider);
+        writeContract = new ethers.Contract(address, abi, signer);
+      } catch (error) {
+        console.error(`Error initializing the ${name} contract : ${error}`);
+      }
+
+      setStates[i]({
+        read: readContract,
+        write: writeContract,
+      });
+    }
   }
 
   return (
@@ -38,11 +101,13 @@ const ContractProvider = ({ children }) => {
         gradingContract,
         studentContract,
         teacherContract,
+        userRole,
+        walletAddress,
       }}
     >
       {children}
     </ContractContext.Provider>
   );
-};
+}
 
 export { ContractContext, ContractProvider };

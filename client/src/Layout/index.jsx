6be49917535004f4c523d28c92dea04c64ae19c9
rowpacmal <rowpacmal@outlook.com>
2024-12-17 17:07:36 +0100

    added test functions to try out add and get teahcers

--- client/src/Layout/index.jsx
+++ client/src/Layout/index.jsx
@@ -1,12 +1,33 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import { AppContext } from '../contexts/AppContext';
 import Navigation from '../components/Navigation';
 import useGradingSystem from '../hooks/useGradingSystem';
+import useTeacherManagement from '../hooks/useTeacherManagement';
+import teacherManagement from '../utils/teacherManagement.config';
+import handleCustomError from '../utils/handleCustomErrors';
 
 function Layout() {
   const { account } = useContext(AppContext);
   const { userRole } = useGradingSystem();
+  const { teacherContract } = useTeacherManagement();
+  const [status, setStatus] = useState('');
+
+  async function handleAddTeacher() {
+    try {
+      await teacherContract.write.registerTeachers([account], [2401]);
+    } catch (error) {
+      setStatus(handleCustomError(teacherManagement.abi, error));
+    }
+  }
+
+  async function handleGetTeachers() {
+    try {
+      console.log(await teacherContract.read.getAllTeachers());
+    } catch (error) {
+      setStatus(handleCustomError(teacherManagement.abi, error));
+    }
+  }
 
   return (
     <>
@@ -17,6 +38,16 @@ function Layout() {
         {account && <p>Wallet: {account}</p>}
 
         <Navigation />
+
+        <button onClick={handleAddTeacher}>Add Teacher</button>
+        <button onClick={handleGetTeachers}>Get Teachers</button>
+
+        {status && (
+          <>
+            <p>{status.name}</p>
+            <p>{status.args}</p>
+          </>
+        )}
       </header>
 
       <main>

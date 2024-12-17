6fe5891b04db84b5c5d8adba7c9d06e3d6889e4d
rowpacmal <rowpacmal@outlook.com>
2024-12-17 17:08:02 +0100

    added teacher contract hook

--- client/src/hooks/useTeacherManagement/index.js
+++ client/src/hooks/useTeacherManagement/index.js
@@ -1,7 +1,18 @@
-import { ethers } from 'ethers';
-import { useMemo } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import teacherManagement from '../../utils/teacherManagement.config';
+import { AppContext } from '../../contexts/AppContext';
+import initializeContract from '../../utils/initializeContract';
 
-function TeacherManagement() {}
+function TeacherManagement() {
+  const { provider, signer } = useContext(AppContext);
+  const [teacherContract, setTeacherContract] = useState(null);
+
+  // Initialize the contract.
+  useEffect(() => {
+    initializeContract(teacherManagement, setTeacherContract, provider, signer);
+  }, [provider, signer]); // Depend on provider and signer.
+
+  return { teacherContract };
+}
 
 export default TeacherManagement;

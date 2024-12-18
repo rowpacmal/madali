76fe725939be230ae71daadb20159bb328181ea5
rowpacmal <rowpacmal@outlook.com>
2024-12-18 14:20:06 +0100

    added add, delete, and update for teachers; added event listener for contract emits

--- client/src/hooks/useTeacherManagement/index.js
+++ client/src/hooks/useTeacherManagement/index.js
@@ -6,32 +6,145 @@ import handleCustomErrors from '../../utils/handleCustomErrors';
 
 function useTeacherManagement() {
   const { ethereum, account, provider, signer } = useContext(AppContext);
-  const [providerError] = useState(
-    'Error getting all teachers: Web3 provider not found.'
-  );
   const [teacherContract, setTeacherContract] = useState(null);
 
   // Initialize the contract.
   useEffect(() => {
-    if (!ethereum || !provider || !signer) {
-      console.error(providerError);
+    if (!ethereum) {
+      console.warn('MetaMask or Ethereum provider not detected.');
+      return;
+    }
 
+    if (!provider) {
+      console.warn('Web3 provider not detected.');
+      return;
+    }
+
+    if (!signer) {
+      console.warn('Web3 signer not detected.');
       return;
     }
 
     initializeContract(teacherManagement, setTeacherContract, provider, signer);
-  }, [ethereum, provider, providerError, signer]); // Depend on ethereum, provider and signer.
+  }, [ethereum, provider, signer]); // Depend on ethereum, provider and signer.
 
-  function checkWeb3Provider() {
-    if (!ethereum || !teacherContract || !account) {
-      console.error(providerError);
+  // Setup event listeners during initialization.
+  useEffect(() => {
+    if (!ethereum) {
+      console.warn('MetaMask or Ethereum provider not detected.');
+      return;
+    }
 
-      return providerError;
+    if (!teacherContract) {
+      console.warn('Teacher contract instance is not initialized.');
+      return;
+    }
+
+    console.info('Setting up event listeners for Teacher contract...');
+
+    setupCourseEventListeners();
+    setupTeacherEventListeners();
+
+    return () => {
+      console.info('Cleaned up Teacher contract event listeners.');
+
+      cleanupCourseEventListeners();
+      cleanupTeacherEventListeners();
+    };
+  }, [ethereum, teacherContract]);
+
+  function dependenciesNullCheck() {
+    if (!ethereum) {
+      console.warn('MetaMask or Ethereum provider not detected.');
+      return;
+    }
+
+    if (!teacherContract) {
+      console.warn('Teacher contract instance is not initialized.');
+      return;
+    }
+
+    if (!account) {
+      console.warn('Account not detected.');
+      return;
     }
   }
 
+  // Course Functions.
+  function setupCourseEventListeners() {
+    // Register course.
+
+    // Delete course.
+    teacherContract.read.on('CourseDeletionFailed_NoCoursesToDelete', () => {
+      console.error(`Error: Teacher has no courses to delete.`);
+    });
+  }
+
+  function cleanupCourseEventListeners() {
+    // Register course.
+
+    // Delete course.
+    teacherContract.read.removeAllListeners(
+      'CourseDeletionFailed_NoCoursesToDelete'
+    );
+  }
+
+  // Teacher Functions.
+  function setupTeacherEventListeners() {
+    // Register teacher.
+    teacherContract.read.on('TeacherAdditionFailed_ZeroAddress', () => {
+      console.error(`Error: Cannot register teacher with zero address.`);
+    });
+
+    teacherContract.read.on(
+      'TeacherAdditionFailed_AlreadyExists',
+      (address) => {
+        console.error(`Error: Teacher with address ${address} already exists.`);
+      }
+    );
+
+    teacherContract.read.on('TeacherRegistered', (teacherAddress) => {
+      console.info(
+        `Success: Teacher with address ${teacherAddress} has been registered.`
+      );
+    });
+
+    // Delete teacher.
+    teacherContract.read.on('TeacherDeletionFailed_ZeroAddress', () => {
+      console.error(`Error: Cannot delete teacher with zero address.`);
+    });
+
+    teacherContract.read.on('TeacherDeletionFailed_NotFound', (address) => {
+      console.error(`Error: Teacher with address ${address} not found.`);
+    });
+
+    teacherContract.read.on('TeacherDeleted', (teacherAddress) => {
+      console.info(
+        `Success: Teacher with address ${teacherAddress} has been deleted.`
+      );
+    });
+  }
+
+  function cleanupTeacherEventListeners() {
+    // Register teacher.
+    teacherContract.read.removeAllListeners(
+      'TeacherAdditionFailed_ZeroAddress'
+    );
+    teacherContract.read.removeAllListeners(
+      'TeacherAdditionFailed_AlreadyExists'
+    );
+    teacherContract.read.removeAllListeners('TeacherRegistered');
+
+    // Delete teacher.
+    teacherContract.read.removeAllListeners(
+      'TeacherDeletionFailed_ZeroAddress'
+    );
+    teacherContract.read.removeAllListeners('TeacherDeletionFailed_NotFound');
+    teacherContract.read.removeAllListeners('TeacherDeleted');
+  }
+
   async function getAllTeachers() {
-    checkWeb3Provider();
+    dependenciesNullCheck();
 
     try {
       const allTeachers = await teacherContract.read.getAllTeachers({
@@ -43,15 +156,12 @@ function useTeacherManagement() {
 
       return teachersArray;
     } catch (error) {
-      const customError = handleCustomErrors(teacherManagement.abi, error);
-      console.error('Error getting all teachers:', customError);
-
-      return customError;
+      return handleCustomErrors(teacherManagement.abi, error);
     }
   }
 
   async function getTeacher(teacherAddress) {
-    checkWeb3Provider();
+    dependenciesNullCheck();
 
     try {
       const teacher = await teacherContract.read.getTeacher(teacherAddress, {
@@ -68,15 +178,12 @@ function useTeacherManagement() {
 
       return teacherObj;
     } catch (error) {
-      const customError = handleCustomErrors(teacherManagement.abi, error);
-      console.error('Error getting a teacher:', customError);
-
-      return customError;
+      return handleCustomErrors(teacherManagement.abi, error);
     }
   }
 
   async function getTotalTeachers() {
-    checkWeb3Provider();
+    dependenciesNullCheck();
 
     try {
       const totalTeachers = Number(
@@ -90,14 +197,62 @@ function useTeacherManagement() {
 
       return totalTeachersNumber;
     } catch (error) {
-      const customError = handleCustomErrors(teacherManagement.abi, error);
-      console.error('Error getting total number of teachers:', customError);
-
-      return customError;
+      return handleCustomErrors(teacherManagement.abi, error);
     }
   }
 
-  return { teacherContract, getAllTeachers, getTeacher, getTotalTeachers };
+  async function deleteTeachers(teachers) {
+    dependenciesNullCheck();
+
+    try {
+      await teacherContract.write.deleteTeachers(teachers, {
+        from: account,
+      });
+
+      console.info('Teachers to be deleted:', teachers);
+    } catch (error) {
+      return handleCustomErrors(teacherManagement.abi, error);
+    }
+  }
+
+  async function registerTeachers(teachers, classes) {
+    dependenciesNullCheck();
+
+    try {
+      await teacherContract.write.registerTeachers(teachers, classes, {
+        from: account,
+      });
+
+      console.info('Teachers to be registered:', teachers);
+    } catch (error) {
+      return handleCustomErrors(teacherManagement.abi, error);
+    }
+  }
+
+  async function updateTeacher(teacher, newClass) {
+    dependenciesNullCheck();
+
+    try {
+      await teacherContract.write.updateTeacher(teacher, newClass, {
+        from: account,
+      });
+
+      console.info('Teachers to be updated:', teacher);
+      console.info('New class to be assigned:', newClass);
+    } catch (error) {
+      return handleCustomErrors(teacherManagement.abi, error);
+    }
+  }
+
+  return {
+    teacherContract,
+    getAllTeachers,
+    getTeacher,
+    getTotalTeachers,
+    deleteTeachers,
+    registerTeachers,
+    updateTeacher,
+  };
 }
 
 export default useTeacherManagement;

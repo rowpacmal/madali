cd254efeb3c61a09aafa0fb69168cf9b6cff9c67
rowpacmal <rowpacmal@outlook.com>
2024-12-19 13:59:40 +0100

    added test functions

--- client/src/pages/Admin/index.jsx
+++ client/src/pages/Admin/index.jsx
@@ -1,8 +1,12 @@
-import { useContext, useEffect } from 'react';
+import axios from 'axios';
+import { useContext, useEffect, useState } from 'react';
 import useStudentManagement from '../../hooks/useStudentManagement';
 import useTeacherManagement from '../../hooks/useTeacherManagement';
 import { UserRoleContext } from '../../contexts/UserRoleContext';
 import { useNavigate } from 'react-router-dom';
+import useGradingSystem from '../../hooks/useGradingSystem';
+import useEducationCertificate from '../../hooks/useEducationCertificate';
+import { AppContext } from '../../contexts/AppContext';
 
 const style = {
   display: 'flex',
@@ -44,7 +48,27 @@ function Admin() {
     registerStudents,
     updateStudent,
   } = useStudentManagement();
+  const {
+    getAllGradesByStudent,
+    getGrade,
+    getTotalGradesByStudent,
+
+    addGrades,
+    deleteGrade,
+    updateGrade,
+  } = useGradingSystem();
+  const {
+    certificateContract,
+
+    getCertificate,
+    getTotalCertificates,
+
+    mintCertificate,
+    updateCertificate,
+  } = useEducationCertificate();
+  const { account } = useContext(AppContext);
   const { userRole } = useContext(UserRoleContext);
+  const [img, setImg] = useState(null);
 
   useEffect(() => {
     if (!userRole) {
@@ -62,48 +86,155 @@ function Admin() {
         <>
           <h2>Admin</h2>
 
-          {/* Grades */}
+          <div>
+            {img && (
+              <img src={img} style={{ width: '100px', height: '100px' }} />
+            )}
+          </div>
+
+          <button
+            onClick={async () => {
+              const totalCert = await getTotalCertificates();
+
+              for (let i = 0; i < totalCert; i++) {
+                const owner = await certificateContract.read.ownerOf(i);
+                console.log('Owner of Certificate ID:', owner);
+
+                if (owner.toLowerCase() === account.toLowerCase()) {
+                  console.log('Found owner');
+                  const uri = await certificateContract.read.tokenURI(3);
+                  const metadata = (await axios.get(uri)).data;
+                  console.log('Metadata:', metadata);
+
+                  metadata && setImg(metadata.image);
+                } else {
+                  console.log('Not found');
+                }
+              }
+            }}
+          >
+            Check Cert Owner
+          </button>
+
+          {/* Certificates */}
           <div style={style}>
-            <h3>Grade Management</h3>
+            <h3>Certificate Management</h3>
 
             <div style={style}>
-              <button onClick={() => {}}>Get All Grades</button>
+              <button
+                onClick={() => {
+                  getCertificate(0);
+                }}
+              >
+                Get Certificate
+              </button>
 
-              <button onClick={() => {}}>Get Grade</button>
-
-              <button onClick={() => {}}>Get Total Number of Grades</button>
+              <button onClick={getTotalCertificates}>
+                Get Total Number of Certificates
+              </button>
             </div>
 
             <div style={style}>
-              <button onClick={() => {}}>Add Grades</button>
+              <button
+                onClick={() => {
+                  mintCertificate(
+                    '0x3c44cdddb6a900fa2b585dd299e03d12fa4293bc',
+                    0,
+                    240103,
+                    'https://cdn-icons-png.flaticon.com/512/6024/6024706.png',
+                    'https://gateway.pinata.cloud/ipfs/bafkreigmjpiryicoxyufhlflfzb5gt2pp2jsnclv4ty7ktj36thad7xpqe'
+                  );
+                }}
+              >
+                Mint Certificate
+              </button>
 
-              <button onClick={() => {}}>Delete Grade</button>
-
-              <button onClick={() => {}}>Update Grade</button>
+              <button
+                onClick={() => {
+                  updateCertificate(
+                    0,
+                    '0x3c44cdddb6a900fa2b585dd299e03d12fa4293bc',
+                    'https://cdn-icons-png.flaticon.com/512/6024/6024708.png'
+                  );
+                }}
+              >
+                Update Certificate
+              </button>
             </div>
           </div>
 
           <hr />
 
-          {/* Classes */}
+          {/* Grades */}
           <div style={style}>
-            <h3>Class Management</h3>
+            <h3>Grade Management</h3>
 
             <div style={style}>
-              <button onClick={getAllClasses}>Get All Classes</button>
+              <button
+                onClick={async () => {
+                  getAllGradesByStudent(
+                    (await getAllStudents((await getAllClasses())[0]))[0]
+                  );
+                }}
+              >
+                Get All Grades
+              </button>
 
-              <button onClick={getTotalClasses}>
-                Get Total Number of Classes
+              <button
+                onClick={async () => {
+                  getGrade(
+                    (
+                      await getAllGradesByStudent(
+                        (
+                          await getAllStudents((await getAllClasses())[0])
+                        )[0]
+                      )
+                    )[0]
+                  );
+                }}
+              >
+                Get Grade
+              </button>
+
+              <button
+                onClick={async () => {
+                  getTotalGradesByStudent(
+                    (await getAllStudents((await getAllClasses())[0]))[0]
+                  );
+                }}
+              >
+                Get Total Number of Grades
               </button>
             </div>
 
             <div style={style}>
-              <button onClick={() => addClass([2401, 2403])}>
-                Add Classes
+              <button
+                onClick={() => {
+                  addGrades(
+                    240103,
+                    1,
+                    ['0x3c44cdddb6a900fa2b585dd299e03d12fa4293bc'],
+                    [4]
+                  );
+                }}
+              >
+                Add Grades
               </button>
 
-              <button onClick={() => deleteClasses([2401, 2403])}>
-                Delete Classes
+              <button
+                onClick={async () => {
+                  deleteGrade(0);
+                }}
+              >
+                Delete Grade
+              </button>
+
+              <button
+                onClick={() => {
+                  updateGrade(0, 6);
+                }}
+              >
+                Update Grade
               </button>
             </div>
           </div>
@@ -176,11 +307,36 @@ function Admin() {
                   updateStudent(
                     '0x3c44cdddb6a900fa2b585dd299e03d12fa4293bc',
                     2401,
-                    2403
+                    2402
                   )
                 }
               >
                 Update Student
+              </button>
+            </div>
+          </div>
+
+          <hr />
+
+          {/* Classes */}
+          <div style={style}>
+            <h3>Class Management</h3>
+
+            <div style={style}>
+              <button onClick={getAllClasses}>Get All Classes</button>
+
+              <button onClick={getTotalClasses}>
+                Get Total Number of Classes
+              </button>
+            </div>
+
+            <div style={style}>
+              <button onClick={() => addClass([2401, 2402])}>
+                Add Classes
+              </button>
+
+              <button onClick={() => deleteClasses([2401, 2402])}>
+                Delete Classes
               </button>
             </div>
           </div>
@@ -289,7 +445,7 @@ function Admin() {
                 onClick={() =>
                   updateTeacher(
                     '0x70997970c51812dc3a010c7d01b50e0d17dc79c8',
-                    2403
+                    2402
                   )
                 }
               >

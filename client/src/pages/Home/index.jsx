2bbb1912ecac0c425acd09b7ee13722a45d2fc77
rowpacmal <rowpacmal@outlook.com>
2024-12-19 16:57:02 +0100

    added test functions to home page

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,5 +1,44 @@
+import Teacher from '../../model/Teacher';
+import useTeacherService from '../../services/useTeacherService';
+
 function Home() {
-  return <h2>Home</h2>;
+  const { getTeachers, addTeacher, updateTeacher } = useTeacherService();
+
+  return (
+    <>
+      <h2>Home</h2>
+
+      <button onClick={getTeachers}>Get Teachers</button>
+
+      <button
+        onClick={() =>
+          addTeacher(
+            new Teacher({
+              id: '0x70997970C51812dc3A010C7d01b50e0d17dc79C8',
+              firstName: 'John',
+              lastName: 'Doe',
+              email: 'V5bK5@example.com',
+              address: '123 Main St',
+              phoneNumber: '555-555-5555',
+            })
+          )
+        }
+      >
+        Add Teacher
+      </button>
+
+      <button
+        onClick={() =>
+          updateTeacher('0x70997970C51812dc3A010C7d01b50e0d17dc79C8', {
+            firstName: 'Jenny',
+            lastName: 'Olson',
+          })
+        }
+      >
+        Update Teacher
+      </button>
+    </>
+  );
 }
 
 export default Home;

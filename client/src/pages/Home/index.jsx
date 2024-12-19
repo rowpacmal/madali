5e52af1384df482817d70445e52b6f7bc94522c8
rowpacmal <rowpacmal@outlook.com>
2024-12-19 17:39:27 +0100

    added student services

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,42 +1,85 @@
+import Person from '../../model/Person';
+import Student from '../../model/Student';
 import Teacher from '../../model/Teacher';
+import useStudentService from '../../services/useStudentService';
 import useTeacherService from '../../services/useTeacherService';
 
 function Home() {
   const { getTeachers, addTeacher, updateTeacher } = useTeacherService();
+  const { getStudents, addStudent } = useStudentService();
 
   return (
     <>
       <h2>Home</h2>
 
-      <button onClick={getTeachers}>Get Teachers</button>
+      <div>
+        <button onClick={getTeachers}>Get Teachers</button>
 
-      <button
-        onClick={() =>
-          addTeacher(
-            new Teacher({
-              id: '0x70997970C51812dc3A010C7d01b50e0d17dc79C8',
-              firstName: 'John',
-              lastName: 'Doe',
-              email: 'V5bK5@example.com',
-              address: '123 Main St',
-              phoneNumber: '555-555-5555',
+        <button
+          onClick={() =>
+            addTeacher(
+              new Teacher({
+                id: '0x70997970C51812dc3A010C7d01b50e0d17dc79C8',
+                firstName: 'John',
+                lastName: 'Doe',
+                email: 'V5bK5@example.com',
+                address: '123 Main St',
+                phoneNumber: '555-555-5555',
+              })
+            )
+          }
+        >
+          Add Teacher
+        </button>
+
+        <button
+          onClick={() =>
+            updateTeacher('0x70997970C51812dc3A010C7d01b50e0d17dc79C8', {
+              firstName: 'Jenny',
+              lastName: 'Olson',
             })
-          )
-        }
-      >
-        Add Teacher
-      </button>
+          }
+        >
+          Update Teacher
+        </button>
+      </div>
 
-      <button
-        onClick={() =>
-          updateTeacher('0x70997970C51812dc3A010C7d01b50e0d17dc79C8', {
-            firstName: 'Jenny',
-            lastName: 'Olson',
-          })
-        }
-      >
-        Update Teacher
-      </button>
+      <br />
+
+      <div>
+        <button onClick={getStudents}>Get Students</button>
+
+        <button
+          onClick={() =>
+            addStudent(
+              new Student({
+                id: '0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC',
+                firstName: 'Billy',
+                lastName: 'Bob',
+                address: '123 Main St',
+                guardians: [
+                  new Person({
+                    firstName: 'Sarah',
+                    lastName: 'Bob',
+                    email: 'V5bK5@example.com',
+                    address: '123 Main St',
+                    phoneNumber: '555-555-5555',
+                  }),
+                  new Person({
+                    firstName: 'Robert',
+                    lastName: 'Bob',
+                    email: 'V5bK5@example.com',
+                    address: '123 Main St',
+                    phoneNumber: '555-555-5555',
+                  }),
+                ],
+              })
+            )
+          }
+        >
+          Add Student
+        </button>
+      </div>
     </>
   );
 }
---
+++ client/src/services/useStudentService/index.js
@@ -0,0 +1,144 @@
+import { useState } from 'react';
+import axios from 'axios';
+import jsonServer from '../../utils/jsonServer.config';
+import Student from '../../model/Student';
+
+function useStudentService() {
+  const [baseURL] = useState(jsonServer.url);
+  const [endpoint] = useState('students');
+
+  async function addStudent(studentData) {
+    try {
+      if (!studentData) {
+        throw new Error('Student data is required');
+      }
+
+      // Check if student already exists.
+      const students = await getStudents();
+
+      if (!students) {
+        throw new Error('Students not found');
+      }
+
+      let exists = false;
+
+      for (let i = 0; i < students.length; i++) {
+        if (students[i].id === studentData.id) {
+          exists = true;
+          break;
+        }
+      }
+
+      // Add student if it doesn't exist.
+      if (!exists) {
+        const response = await axios.post(
+          `${baseURL}/${endpoint}`,
+          new Student(studentData)
+        );
+        const data = await response.data;
+        console.info('Student:', data);
+
+        return data;
+      }
+
+      // Throw error if student already exists.
+      throw new Error('Student already exists');
+    } catch (error) {
+      console.error('Error adding student:', error);
+      return error;
+    }
+  }
+
+  async function deleteStudent(id) {
+    try {
+      if (!id) {
+        throw new Error('Student ID is required');
+      }
+
+      const response = await axios.delete(`${baseURL}/${endpoint}/${id}`);
+      const data = await response.data;
+      console.info('Student:', data);
+
+      return data;
+    } catch (error) {
+      console.error('Error deleting student:', error);
+      return error;
+    }
+  }
+
+  async function getStudents() {
+    try {
+      const response = await axios.get(`${baseURL}/${endpoint}`);
+      const data = await response.data;
+      console.info('Students:', data);
+
+      return data;
+    } catch (error) {
+      console.error('Error getting students:', error);
+      return error;
+    }
+  }
+
+  async function getStudent(id) {
+    try {
+      if (!id) {
+        throw new Error('Student ID is required');
+      }
+
+      const response = await axios.get(`${baseURL}/${endpoint}/${id}`);
+      const data = await response.data;
+      console.info('Student:', data);
+
+      return data;
+    } catch (error) {
+      console.error('Error getting student:', error);
+      return error;
+    }
+  }
+
+  async function updateStudent(id, updateData) {
+    try {
+      if (!id) {
+        throw new Error('Student ID is required');
+      }
+
+      if (!updateData) {
+        throw new Error('Update data is required');
+      }
+
+      const student = await getStudent(id);
+
+      if (!student) {
+        throw new Error('Student not found');
+      }
+
+      const fieldsToUpdate = Object.entries(updateData);
+
+      for (let i = 0; i < fieldsToUpdate.length; i++) {
+        if (student[fieldsToUpdate[i][0]]) {
+          student[fieldsToUpdate[i][0]] = fieldsToUpdate[i][1];
+        } else {
+          console.warn(
+            `Field ${fieldsToUpdate[i][0]} does not exist in student`
+          );
+        }
+      }
+
+      const response = await axios.put(
+        `${baseURL}/${endpoint}/${id}`,
+        new Student(student)
+      );
+      const data = await response.data;
+      console.info('Student:', data);
+
+      return data;
+    } catch (error) {
+      console.error('Error updating student:', error);
+      return error;
+    }
+  }
+
+  return { addStudent, deleteStudent, getStudents, getStudent, updateStudent };
+}
+
+export default useStudentService;
